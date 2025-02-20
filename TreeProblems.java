7e46a0ee909e28409a795f00328038f73d7fd424
ashad0000 <120233711+ashad0000@users.noreply.github.com>
2025-02-20 12:11:47 -0600

    Add files via upload

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,8 +1,8 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Abdul Shad / 272 ***
  *
  * This java file contains several simple tree problems that need to be
- * codified. These routines  must use the TreeMap and TreeSet library
+ * codified. These routines must use the TreeMap and TreeSet library
  * classes from the Java Collection Framework.
  *
  */
@@ -11,54 +11,54 @@ import java.util.*;
 
 public class TreeProblems {
 
-  /**
-   * Method different()
-   *
-   * Given two TreeSets of integers, return a TreeSet containing all elements 
-   * that are NOT in both sets. In other words, return a TreeSet of all the
-   * elements that are in one set but not the other.
-   */
-  
-  public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+    /**
+     * Method different()
+     *
+     * Given two TreeSets of integers, return a TreeSet containing all elements
+     * that are NOT in both sets. In other words, return a TreeSet of all the
+     * elements that are in one set but not the other.
+     */
+    public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+        // Create a new TreeSet to store unique elements
+        Set<Integer> uniqueElements = new TreeSet<>(setA);
+        uniqueElements.addAll(setB); // Union of both sets
 
-    // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
-    //
-    // This can be done numerous ways, but once such will only that
-    // *several* lines of code. Hint: create two temporary TreeSets and utilize the
-    // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
+        // Find common elements
+        Set<Integer> commonElements = new TreeSet<>(setA);
+        commonElements.retainAll(setB); // Intersection of both sets
 
-    return setA;
-  }
+        // Remove common elements to get the difference
+        uniqueElements.removeAll(commonElements);
 
+        return uniqueElements;
+    }
 
-  /**
-   * Method removeEven()
-   *
-   * Given a treeMap with the key as an integer, and the value as a String,
-   * remove all <key, value> pairs where the key is even. 
-   */
+    /**
+     * Method removeEven()
+     *
+     * Given a treeMap with the key as an integer, and the value as a String,
+     * remove all <key, value> pairs where the key is even.
+     */
+    public static void removeEven(Map<Integer, String> treeMap) {
+        // Use an iterator to avoid ConcurrentModificationException
+        Iterator<Integer> iterator = treeMap.keySet().iterator();
 
-  public static void removeEven(Map<Integer, String> treeMap) {
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove(); // Remove even keys
+            }
+        }
+    }
 
-    // INSERT CODE HERE.
+    /**
+     * Method treesEqual()
+     *
+     * Given two treeMaps, each with the key as an integer, and the value as a String,
+     * return a boolean value indicating if the two trees are equal or not.
+     */
+    public boolean treesEqual(Map<Integer, String> tree1, Map<Integer, String> tree2) {
+        // Compare the two maps directly
+        return tree1.equals(tree2);
+    }
 
-    return;
-  }
-
-
-  /**
-   * Method treesEqual()
-   *
-   * Given two treeMaps, each with the key as an integer, and the value as a String,
-   * return a boolean value indicating if the two trees are equal or not.
-   */
-
-  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
-
-    // INSERT CODE HERE
-
-    return false;
-
-  }
-
-} // end treeProblems class
+} // end TreeProblems class
